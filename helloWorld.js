4db6f44325c08cf5be3088cae1615019d28f90a8
AmoDinho <u14284783@tuks.co.za>
2021-03-11 21:26:26 +0200

    lets see

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,13 @@
 import { URKBase } from "./airtable";
 
-const helloWorld = async () => {};
+const helloWorld = async () => {
+  URKBase.base("Web Development")
+    .select({
+      view: "Main View",
+    })
+    .eachPage((records) => {
+      console.log("Name", records);
+    });
+};
+
+helloWorld();
