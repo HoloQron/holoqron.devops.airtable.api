c374c7b351c7238a854da30bcc1d8a5573ec68b6
AmoDinho <u14284783@tuks.co.za>
2021-03-11 21:21:46 +0200

    exporting the base

--- airtable/index.js
+++ airtable/index.js
@@ -1,5 +1,8 @@
 const Airtable = require("airtable");
-Airtable.configure({
+const initAirtable = new Airtable.configure({
   endpointUrl: "https://api.airtable.com",
   apiKey: process.env.AIRTABLE_API_KEY,
 });
+
+const URKBase = initAirtable.base("appjGlK4KwN8XTOKA");
+export { URKBase };
